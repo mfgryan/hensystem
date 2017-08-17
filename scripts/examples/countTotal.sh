7492b70b100067ae01b3fd3273356d23b685fd4d
mfgryan <ryanbhenao@gmail.com>
2017-08-16 23:56:39 -0400

    cleaned up awk script

--- scripts/examples/countTotal.sh
+++ scripts/examples/countTotal.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-cat $1 | awk '
+# reads the companies file and counts the totals
+awk '
     BEGIN { FS="," }
-    { amounts[$2]+=$4; counts[$2]++}
-    END { for(i in amounts) print i, amounts[i]/counts[i] }
-'
+    { 
+        amounts[$2]+=$4; 
+        counts[$2]++
+    }
+    END { 
+        for(i in amounts){ 
+            print i, amounts[i]/counts[i]; 
+        }
+    }
+' ./companies
